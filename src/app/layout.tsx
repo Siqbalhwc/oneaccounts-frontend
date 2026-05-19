2486b783f211df26e88542869628b8f7ffe91fb6
Shahid Iqbal <siqbalhwc@gmail.com>
2026-05-19 07:20:11 +0500

    Fix sidebar BANKING/SYSTEM visibility and polish invoice/bill pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
-import { ThemeProvider } from "@/contexts/ThemeContext"
+// import { ThemeProvider } from "@/contexts/ThemeContext"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <ThemeProvider>
+        {/* <ThemeProvider> */}
           {children}
-        </ThemeProvider>
+        {/* </ThemeProvider> */}
       </body>
     </html>
   )
